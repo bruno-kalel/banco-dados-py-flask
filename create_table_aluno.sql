b58f84cc1c7718604a8d6519faf8e81d9df9559b
Bruno Kalel <104171372+bruno-kalel@users.noreply.github.com>
2023-09-06 18:11:24 -0300

    remoção de dados sensíveis

--- create_table_aluno.sql
+++ create_table_aluno.sql
@@ -14,4 +14,4 @@ create table aluno
 );
 
 INSERT INTO aluno(cpf, matricula, nome_inicio, nome_resto, data_nascimento, nome_curso, tipo_curso, codigo_turma, email_pessoal, email_institucional, email_lista_transmissao)
-VALUES (12312312300, 11223344, 'bruno', 'I', '2001-01-01', 'Ciência Computação', 'Graduação', 'CC4TA', 'nome@icloud.com', 'nome@aluno.cesupa.br', 'curso@aluno.cesupa.br'), (11122233, 44112233, 'bruno', 'II', '2001-01-01', 'Ciência Computação', 'Graduação', 'CC4TA', 'nome@icloud.com', 'nome@aluno.cesupa.br', 'curso@aluno.cesupa.br')
+VALUES (12312312300, 11223344, 'bruno', 'primeiro', '1953-01-01', 'Ciência Computação', 'Graduação', 'CC4TA', 'nome@icloud.com', 'nome@aluno.faculdade.br', 'curso@aluno.faculdade.br'), (11122233, 44112233, 'bruno', 'segundo', '1953-01-01', 'Ciência Computação', 'Graduação', 'CC4TA', 'nome@icloud.com', 'nome@aluno.faculde.br', 'curso@aluno.faculdade.br')
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import frontend
 
-pesquisar_por = 'bruno'
-
 if __name__ == '__main__':
     frontend.app.debug = True
     frontend.app.run()
